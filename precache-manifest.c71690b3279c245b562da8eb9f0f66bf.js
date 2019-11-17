2e6236977eda3da62a23ba104259fc19d0194444
Marcelina Wajdzik <marcelawa20@gmail.com>
2019-11-17 16:40:40 +0100

    Updates

---
+++ precache-manifest.c71690b3279c245b562da8eb9f0f66bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26531c440d30bc9994f8dd0f753fd932",
+    "url": "/MarcelaWajdzik.github.io/Bitcoin-react/index.html"
+  },
+  {
+    "revision": "722c9fbf2a41fc893701",
+    "url": "/MarcelaWajdzik.github.io/Bitcoin-react/static/css/main.7d1065e9.chunk.css"
+  },
+  {
+    "revision": "bf0c5e164f97931b5764",
+    "url": "/MarcelaWajdzik.github.io/Bitcoin-react/static/js/2.5ab8e81e.chunk.js"
+  },
+  {
+    "revision": "722c9fbf2a41fc893701",
+    "url": "/MarcelaWajdzik.github.io/Bitcoin-react/static/js/main.555fddcd.chunk.js"
+  },
+  {
+    "revision": "f5d20de6a1a8437bc337",
+    "url": "/MarcelaWajdzik.github.io/Bitcoin-react/static/js/runtime-main.0ed9e58d.js"
+  }
+]);
